1ef839158cdce420216798ebba8bbee63f8449d2
Stijnvaneven <Stijn.vaneven@nuance.com>
2015-11-08 12:06:24 -0500

    Add plot4.r and its bitmap.

---
+++ plot4.r
@@ -0,0 +1,35 @@
+# create plot #4
+
+# file to get data and read the data
+dataFileName <- "household_power_consumption.txt"
+HPC<-read.table(dataFileName, header = TRUE, sep=";", na.strings = "?", stringsAsFactors=FALSE)
+
+#select rows for Date = '1/2/2007' OR Date = '2/2/2007'
+HPC.SubsetDates<-subset(HPC, Date == "1/2/2007"| Date == "2/2/2007")
+
+#combine date and time and format as asked by assignment
+datetime <- strptime(paste(HPC.SubsetDates$Date, HPC.SubsetDates$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+# convert into numerics
+globalActivePower <- as.numeric(HPC.SubsetDates$Global_active_power)
+globalReactivePower <- as.numeric(HPC.SubsetDates$Global_reactive_power)
+voltage <- as.numeric(HPC.SubsetDates$Voltage)
+subMetering1 <- as.numeric(HPC.SubsetDates$Sub_metering_1)
+subMetering2 <- as.numeric(HPC.SubsetDates$Sub_metering_2)
+subMetering3 <- as.numeric(HPC.SubsetDates$Sub_metering_3)
+
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime, subMetering1, type="l", xlab="", ylab="Energy sub metering" )
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global Reactive Power")
+
+dev.off()
